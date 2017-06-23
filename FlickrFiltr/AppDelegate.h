85c69120e81041278d412751f71ff401bc75be19
Admin <admin@Mac-Admin.local>
2017-06-23 16:09:40 +0300

    Initial Commit

---
+++ FlickrFiltr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrFiltr
+//
+//  Created by Admin on 23.06.17.
+//  Copyright © 2017 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
